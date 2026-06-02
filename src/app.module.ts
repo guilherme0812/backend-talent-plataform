5613a4835cb734b1b0bc095f51915d83eff8f18b
guilherme0812 <guilherme09112001@gmail.com>
2026-06-02 16:43:30 +0200

    start talent module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TalentModule } from './modules/talent/talent.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [TalentModule],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/modules/talent/talent.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { TalentService } from './talent.service';
+
+@Controller()
+export class TalentController {
+    constructor(readonly talentService: TalentService) { }
+
+    @Get('test')
+    getTest(): string {
+        return this.talentService.getHello();
+    }
+}
